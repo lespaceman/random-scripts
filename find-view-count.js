90bfd974e88d809958b75d61b8725bc09d8e14ca
nadeem@revlity.com <n.adeem@outlook.com>
2021-09-19 21:03:52 +0530

    find view count

--- find-view-count.js
+++ find-view-count.js
@@ -1,11 +1,22 @@
 
 const csv = require('csv-parser')
+const ObjectsToCsv = require('objects-to-csv');
 const fs = require('fs')
-const results = [];
 
+const results = [];
 fs.createReadStream('input/content_usage.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-        console.log(results);
+        const counts = {};
+        results.forEach(usage => {
+            let asstetId = usage.asset_id;
+            counts[asstetId] = (counts[asstetId] || 0) + 1;
+        });
+        console.log(counts);
+        let countArr = [counts];
+        const csv = new ObjectsToCsv(countArr);
+        csv.toDisk('output/view_counts.csv');
     });
+
+
